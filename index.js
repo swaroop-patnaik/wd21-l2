65f241ca0623c9b3b8ec097dc60c7996e8082720
swaroop-patnaik <113629577+swaroop-patnaik@users.noreply.github.com>
2022-11-16 20:55:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const http=require("http");
+const fs=require("fs");
+const args=require("minimist")(process.argv.slice(2));
+
+let homeCxt="";
+let projectCxt="";
+let regCxt="";
+fs.readFile("home.html",(err,home)=>{
+    if(err){
+        throw err;
+    }
+    homeCxt=home;
+});
+
+fs.readFile("project.html",(err,project)=>{
+    if(err){
+        throw err;
+    }
+    projectCxt=project;
+});
+
+fs.readFile("registration.html",(err,registration)=>{
+    if(err){
+        throw err;
+    }
+    regCxt=registration;
+});
+
+http
+ .createServer((request,response)=>{
+    let url=request.url;
+    response.writeHeader(200,{"Content-Type":"text/html"});
+    switch(url){
+        case "/project":
+            response.write(projectCxt);
+            response.end();
+            break;
+        case "/registration":
+            response.write(regCxt);
+            response.end();
+            break;
+        default:
+             response.write(homeCxt);
+             response.end();
+             break;
+    }
+ })
+ .listen(args["port"]);
